7c0984f0b302b8770f316b35e6f0107ce13362ab
liwenjie <wjli@wisedu.com>
2017-03-15 20:34:09 +0800

    单独抽出config

---
+++ config.js
@@ -0,0 +1,20 @@
+const config = {
+    DOM: '.text img',
+    downloadurl: '/Users/admin/meizi/',
+    headers: {
+        url: 'http://jandan.net/ooxx/page-1973',
+        gzip: true, //解决编码为乱码
+        headers: {
+            'Connection': 'keep-alive',
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.110 Safari/537.36',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+            'Accept-Encoding': 'gzip, deflate, sdch',
+            'Accept-Language': 'zh-CN,zh;q=0.8',
+            'Cache-Control': 'max-age=0',
+            'Host': 'jandan.net',
+            'Upgrade-Insecure-Requests': '1',
+            'accept-charset': 'gb2312'
+        }
+    }
+}
+module.exports = config;
