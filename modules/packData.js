cdf85b41ba23b90e1d96906a80db64bf118742ed
liwenjie <wjli@wisedu.com>
2017-03-16 16:53:35 +0800

    包装imgsarr

--- modules/packData.js
+++ modules/packData.js
@@ -2,7 +2,7 @@ const cheerio = require('cheerio');
 let packData = (data) => {
     const $ = cheerio.load(data);
     let imgsArr = $('.text img').toArray();
-    imgsArr = imgsArr.map(img => {
+    return imgsArr.map(img => {
         return {
             src: img.attribs.src
         };
