bd3e22890db2371b7d189874b538138cb50e71d1
liwenjie <wjli@wisedu.com>
2017-03-16 09:05:27 +0800

    包装一个promis来请求页面，返回结果

---
+++ modules/requestP.js
@@ -0,0 +1,22 @@
+const request = require('request');
+
+let requestP = (headers) => {
+    return new Promise((reslove, reject) => {
+        request(headers, (error, res, body) => {
+            if (error) {
+                reject({
+                    error
+                });
+            } else if (res.statusCode !== 200) {
+                reject({
+                    res
+                });
+            } else {
+                reslove(body);
+            }
+        });
+    });
+}
+
+
+module.exports = requestP;
