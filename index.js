ff78bfbd3bd241a1727186d4564ce658048ec09f
liwenjie <wjli@wisedu.com>
2017-03-16 17:24:09 +0800

    完整逻辑： 创建图片文件夹=> 请求页面=> 包装数据=> 遍历数据数组，下载

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
-const {headers} = require('./config.js');
+const fs = require('fs');
 
-const requestP = require('./modules/requestP.js');
+const {headers, downloadPath} = require('./config.js');
+const {requestP} = require('./modules/request.js');
 const packData = require('./modules/packData.js');
+const download = require('./modules/download.js');
 
 let main = async () => {
+    fs.exists(downloadPath, exists => {
+        if (!exists) {
+            fs.mkdir(downloadPath);
+        }
+    })
     try {
         let data = await requestP(headers);
         let imgsurlArr = packData(data);
+        imgsurlArr.map(img => {
+            download(img.src).then((fileName) => {
+                console.log(`success: ${fileName}`);
+            });
+        });
     } catch (e) {
-        console.log(e);
+        console.log(`[error]: ${JSON.stringify(e)}`);
     }
 };
 
