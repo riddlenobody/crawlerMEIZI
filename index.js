a1a547a90555b96f675c498e22399d61b09ff0c6
liwenjie <wjli@wisedu.com>
2017-03-16 15:32:50 +0800

    得到图片url

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const {headers} = require('./config.js');
 
 const requestP = require('./modules/requestP.js');
-// const packData = require('./modules/packData.js');
+const packData = require('./modules/packData.js');
 
 let main = async ()=> {
     try {
         let data = await requestP(headers);
-        console.log(data);
+        let imgsurlArr = packData(data);
     }catch(e){
         console.log(e);
     }
